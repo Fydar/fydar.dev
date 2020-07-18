3005a44dbb8641aa67850bdca9ad756d69e2c859
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-07-18 13:46:58 +0100

    💄 Render the available portfolio items on the home page
    This is done using "ViewComponents"

---
+++ src/Portfolio.Instance/ViewComponents/PortfolioItem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Threading.Tasks;
+
+namespace Portfolio.Instance.ViewComponents
+{
+    public class PortfolioItem : ViewComponent
+    {
+        public PortfolioItem()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ProjectModel projectModel)
+        {
+            return View(projectModel);
+        }
+    }
+}
