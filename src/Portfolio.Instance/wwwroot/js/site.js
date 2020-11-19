a5f44ddf2e84a1fcf6182f928304bf8438a0f607
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-11-19 01:49:32 +0000

    📝 Added vector models to the RPGCore project page

--- src/Portfolio.Instance/wwwroot/js/site.js
+++ src/Portfolio.Instance/wwwroot/js/site.js
@@ -1,44 +1,23 @@
 ﻿const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 const invlerp = (x, y, a) => clamp((a - x) / (y - x));
-const lerp = function (value1, value2, amount) {
-	amount = amount < 0 ? 0 : amount;
-	amount = amount > 1 ? 1 : amount;
-	return value1 + (value2 - value1) * amount;
-};
 
-window.addEventListener("scroll", updateParallax);
-window.addEventListener("resize", updateParallax);
-updateParallax();
+function UpdateRelativeElements() {
+	var fsaRelativeObjects = document.getElementsByClassName("fsa-relative");
 
-(function () {
-	updateParallax();
-})();
+	for (let p = 0; p < fsaRelativeObjects.length; p++) {
+		var fsaRelative = fsaRelativeObjects[p];
 
-function updateParallax() {
-	var paralaxContainers = document.getElementsByClassName("parallax-container");
+		var bounds = fsaRelative.getBoundingClientRect();
+		var scroll = invlerp(-bounds.height, screen.height, bounds.top);
 
-	for (let p = 0; p < paralaxContainers.length; p++) {
-		var paralaxContainer = paralaxContainers[p];
-
-		var offset = 0;
-		if (paralaxContainer.classList.contains("parallax-anchor-top")) {
-			offset = screen.height * 0.25;
-		}
-
-		var bounds = paralaxContainer.getBoundingClientRect();
-		var scroll = invlerp(-bounds.height, screen.height, bounds.top + offset);
-
-		for (let i = 0; i < paralaxContainer.children.length; i++) {
-			var layer = paralaxContainer.children[i];
-
-			if (layer.classList.contains("parallax-layer")) {
-
-				var layerBound = layer.getBoundingClientRect();
-
-				var offset = lerp(0, -layerBound.height + bounds.height, scroll);
-
-				layer.style.transform = "translateY(" + offset + "px)";
-			}
-		}
+		fsaRelative.style.setProperty("--scroll", scroll);
 	}
 }
+
+window.addEventListener("scroll",
+	() => { UpdateRelativeElements(); }, false
+);
+window.addEventListener("resize",
+	() => { UpdateRelativeElements(); }, false
+);
+UpdateRelativeElements();
