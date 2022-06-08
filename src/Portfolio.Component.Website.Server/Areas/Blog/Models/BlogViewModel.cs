5deacf0f24dd9a42dc55388b91f3d515b6829ab2
Anthony Marmont <dev.anthonymarmont@gmail.com>
2022-06-08 01:50:53 +0100

    🐛 Fixing namespaces

--- src/Portfolio.Component.Website.Server/Areas/Blog/Models/BlogViewModel.cs
+++ src/Portfolio.Component.Website.Server/Areas/Blog/Models/BlogViewModel.cs
@@ -1,14 +1,13 @@
 using Portfolio.Services.Content.Blog;
 
-namespace Portfolio.Component.Website.Server.Areas.Blog.Models
-{
-	public class BlogPostViewModel
-	{
-		public BlogPostModel BlogPost { get; set; }
+namespace Portfolio.Component.Website.Server.Areas.Blog.Models;
 
-		public BlogPostViewModel(BlogPostModel blogPost)
-		{
-			BlogPost = blogPost;
-		}
+public class BlogPostViewModel
+{
+	public BlogPostModel BlogPost { get; set; }
+
+	public BlogPostViewModel(BlogPostModel blogPost)
+	{
+		BlogPost = blogPost;
 	}
 }
