5369f10586d39c1803d3fe4efc0caa37a5975e90
fydar <fydar.dev@gmail.com>
2024-10-22 12:03:16 +0100

    📦 Updating Lost in Static to remove splashscreen

--- src/Fydar.Dev.WebApp/wwwroot/play/lostinstatic/ServiceWorker.js
+++ src/Fydar.Dev.WebApp/wwwroot/play/lostinstatic/ServiceWorker.js
@@ -1,10 +1,10 @@
-const cacheName = "Fydar-Lost in Static-WebGL";
+const cacheName = "fydar-lostinstatic-v1.0.0";
 const contentToCache = [
     "./",
-    "Build/WebGL.loader.js",
-    "Build/WebGL.framework.js.br",
-    "Build/WebGL.data.br",
-    "Build/WebGL.wasm.br"
+    "build/lostinstatic.loader.js",
+    "build/lostinstatic.framework.js.br",
+    "build/lostinstatic.data.br",
+    "build/lostinstatic.wasm.br"
 ];
 
 self.addEventListener('install', function (e) {
